81a04c64d6e583abcbdb3fd122333671e0b255ce
Suyash-codess <suyeshtiwari2007@gmail.com>
2026-07-01 04:31:10 +0530

    fix nextauth session id mapping

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -60,15 +60,9 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     }),
   ],
   callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id;
-      }
-      return token;
-    },
     async session({ session, token }) {
-      if (token && session.user) {
-        session.user.id = token.id as string;
+      if (token?.sub && session.user) {
+        session.user.id = token.sub;
       }
       return session;
     },
